b60e5e813758a35fe045c896d0b4108071365d42
eduhenripontes <edu.henri@gmail.com>
2021-12-30 19:22:08 -0300

    Encadeamento de No e Generics

---
+++ src/main/java/com/projetono/No.java
@@ -0,0 +1,35 @@
+package com.projetono;
+
+public class No<T> {
+//código feito primeiro a partir de conceito e encadeamento de No. Depois foi implementado o Generics
+    private T conteudo;
+    private No<T> proximoNo;
+
+    public No(T conteudo) {
+        this.proximoNo = null;
+        this.conteudo = conteudo;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No<T> getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setProximoNo(No<T> proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "conteudo='" + conteudo + '\'' +
+                '}';
+    }
+}
